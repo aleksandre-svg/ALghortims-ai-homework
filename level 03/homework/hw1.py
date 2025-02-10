fd76a30d365bae7e2d9e4f64c87905a92388c160
Diana044 <dzukaevadiana3@gmail.com>
2025-02-10 19:44:46 +0400

    commit

---
+++ level 03/homework/hw1.py
@@ -0,0 +1,12 @@
+my_list = [53, 34, 46, 75, 34]
+largest = my_list[0]
+if my_list[1]>largest:
+    largest=my_list[1]
+if my_list[2]>largest:
+    largest=my_list[2]
+if my_list[3]>largest:
+    largest=my_list[3]
+if my_list[4]>largest:
+    largest=my_list[4]
+print(largest)
+#ამის და ფსეუოდს გარდა ვერაფერი გავიგე
